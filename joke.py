beea3c9b9d1c912a54f4a79ebedf7c3e4d7474a3
charu <charukashyap290@gmail.com>
2026-05-24 23:39:30 +0530

    update program

--- joke.py
+++ joke.py
@@ -3,3 +3,11 @@ import pyjokes
 print("printimg jokes....")
 joke=pyjokes.get_joke()
 print(joke)
+
+
+
+"""
+output:
+printimg jokes....
+The program is absolutely right; therefore the computer must be wrong.
+"""
