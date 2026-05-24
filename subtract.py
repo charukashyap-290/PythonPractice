60e32c03f437e9c758768979f461ca92970f3153
charu <charukashyap290@gmail.com>
2026-05-24 18:25:33 +0530

    update program

---
+++ subtract.py
@@ -0,0 +1,8 @@
+# Python program to subtract two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+difference = num1 - num2
+
+print("The difference is:", difference)
