653c7a65d91a51d06701cc8ad1e702ed326ca5a1
charu <charukashyap290@gmail.com>
2026-05-24 19:18:36 +0530

    update program

--- add.py
+++ add.py
@@ -6,3 +6,11 @@ num2 = float(input("Enter second number: "))
 sum = num1 + num2
 
 print("The sum is:", sum)
+
+
+
+
+#outpput:
+#Enter first number: 21
+#Enter second number: 96
+#The sum is: 117.0
