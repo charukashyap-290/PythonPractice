771127e178198b0ab04a38b654913eeaa4de0bd3
charu <charukashyap290@gmail.com>
2026-05-24 18:53:40 +0530

    update program

---
+++ divide.py
@@ -0,0 +1,8 @@
+# Python program to divide two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = num1 / num2
+
+print("Division =", result)
