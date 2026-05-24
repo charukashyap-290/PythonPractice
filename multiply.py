c301dd52572ff375b181013ea04d877b9bebc72a
charu <charukashyap290@gmail.com>
2026-05-24 18:43:56 +0530

    update program

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Python program to multiply two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+product = num1 * num2
+
+print("The product is:", product)
