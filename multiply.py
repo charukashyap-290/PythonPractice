44633f4c4600c0015fe4682d06bc51fd5ef15395
charu <charukashyap290@gmail.com>
2026-05-24 19:24:58 +0530

    update program

--- multiply.py
+++ multiply.py
@@ -6,3 +6,11 @@ num2 = float(input("Enter second number: "))
 product = num1 * num2
 
 print("The product is:", product)
+
+
+
+
+#output:
+#Enter first number: 543
+#Enter second number: 54
+#The product is: 29322.0
