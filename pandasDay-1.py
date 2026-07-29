0de08b090db9ec2b4cfb60b5455feeb0a82a7020
charu <charukashyap290@gmail.com>
2026-07-29 10:44:03 +0530

    update program

---
+++ pandasDay-1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df=pd.DataFrame({"name ":["Vanshika","Charu"], "marks":[95,100]})
+print(df)
+
+
+
+
+
+'''  name   marks
+0  Vanshika     95
+1     Charu    100'''
--- variable.py
+++ variable.py
@@ -5,7 +5,4 @@ print("hello i am",name)
 print("i am",age,"years old")
 
 
-
-
-'''hello i am charu
-i am 20 years old'''
+      
