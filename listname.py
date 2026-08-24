27877dbdd9b8dbab9ef59f95692cea72e83b02cd
charu <charukashyap290@gmail.com>
2026-08-24 11:58:29 +0530

    update program

---
+++ listname.py
@@ -0,0 +1,8 @@
+names = ["Rahul", "Aman", "Riya", "Priya", "Neha", "Anjali", "Karan"]
+
+name = input("Enter a name: ")
+
+if name in names:
+    print("Name is present in the list.")
+else:
+    print("Name is not present in the list.")
